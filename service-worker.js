2c7e7fe5f07242a1ff0c083cd7540c8128972308
urielxdpro25 <urieluncos@gmail.com>
2026-07-13 18:16:36 -0300

    fix del manifest, en la start_url juego.html --> Index.html

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Versión del caché
 // ==========================================
 
-const CACHE = "gestionador-v6";
+const CACHE = "gestionador-v7";
 
 // Archivos que se guardarán para usar sin Internet
 const ARCHIVOS = [
